556ffb863a87c0e4cc037aa5de16a2ad9b4829e1
crjostad <crjostad@students.unwsp.edu>
2024-09-17 11:38:34 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -15,14 +15,14 @@ def weight_conversion(weight):
     if weight <= 2:
         print("$", weight*1.5)
     
-elif weight <= 6 > 2:
-    print("$", weight*3)
+    elif weight <= 6 > 2:
+        print("$", weight*3)
     
-elif weight <= 10 > 6:
-    print("$",  weight*4)
+    elif weight <= 10 > 6:
+        print("$",  weight*4)
     
-else:
-    print("$", weight*4.75)
+    else:
+        print("$", weight*4.75)
    
     
     return shippingCost
