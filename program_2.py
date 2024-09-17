f4deb7e9273f5cc15e2d35465a1be0e75c3bdf80
crjostad <crjostad@students.unwsp.edu>
2024-09-17 10:04:36 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -5,7 +5,7 @@
 # If the person is at least 13 years old, but less than 20 years old, it should display "teenager".
 # If the person is at least 20 year old, it should display "adult".
 
-age = float(input("Enter the your age: "))
+age = float(input("Enter your age: "))
 
 if age <= 1:
     print("You are an infant")
